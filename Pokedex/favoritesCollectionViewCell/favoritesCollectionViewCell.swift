2d00708ee76d5a6744252386f67cf909292dc5c3
felipecoast <felipe.coast@gmail.com>
2022-09-07 21:16:42 -0300

    commit inicial favoritos

---
+++ Pokedex/favoritesCollectionViewCell/favoritesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  favoritesCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Felipe Almeida on 07/09/22.
+//
+
+import UIKit
+
+class favoritesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
