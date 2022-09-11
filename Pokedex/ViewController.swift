a5fb50c829afd620a7a00451c3d25744ba768c9a
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-09-11 19:12:53 -0300

    create home colection view cell

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
+class MyTabBarConstroller: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTabBar()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tabBar.frame.size.height = 110
+        tabBar.frame.origin.y = view.frame.height - 110
+    }
+    
+    func configTabBar() {
+        tabBar.layer.cornerRadius = 40
+        tabBar.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        tabBar.layer.borderWidth = 2.5
+        tabBar.layer.borderColor = UIColor.black.cgColor
+        tabBar.clipsToBounds = true
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
