f8036ff97f6a928c60a9d283d62392b210ca6044
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-09-12 19:55:13 -0300

    foi criada tela de navegacao da tela login com tela Home

--- Pokedex/Login/LoginViewController.swift
+++ Pokedex/Login/LoginViewController.swift
@@ -7,23 +7,61 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTxtField: UITextField!
+    @IBOutlet weak var passWordTxtField: UITextField!
+    @IBOutlet weak var entrarButton: UIButton!
+    @IBOutlet weak var cadastrarButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        emailTxtField.delegate = self
+        passWordTxtField.delegate = self
+        corneBotoes()
+        botaoDesativado()
+        view.backgroundColor = UIColor(red: 118/255, green: 204/255, blue: 232/255, alpha: 1.0)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
     }
-    */
+
+    @IBAction func didTapLoginButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewcontroler = storyboard.instantiateViewController(withIdentifier: "TabBar")
+        navigationController?.pushViewController(viewcontroler, animated: true)
+    }
+    
+    func botaoDesativado() {
+        if emailTxtField.text == "" || passWordTxtField.text == "" {
+            entrarButton.isEnabled = false
+            entrarButton.backgroundColor = UIColor(red: 92/255, green: 94/255, blue: 100/255, alpha: 1.0)
+        } else {
+            entrarButton.isEnabled = true
+            entrarButton.backgroundColor = UIColor(red: 29/255, green: 44/255, blue: 94/255, alpha: 1.0)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.isEqual(self.emailTxtField) {
+            self.emailTxtField.becomeFirstResponder()
+            self.passWordTxtField.becomeFirstResponder()
+        } else {
+            self.passWordTxtField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        botaoDesativado()
+    }
+    
+    func corneBotoes() {
+        entrarButton.layer.cornerRadius = 19
+        cadastrarButton.layer.cornerRadius = 19
+    }
 
 }
+
