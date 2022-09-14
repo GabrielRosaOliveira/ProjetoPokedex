69560aa6805a84f75965ec13fbad1afb4bdd9fa3
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-09-14 19:30:47 -0300

    montei a tela de Loading

---
+++ Pokedex/Loading/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  Pokedex
+//
+//  Created by Gabriel on 14/09/22.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
