e0d79efd51faae20e7c23cfa0e9f5b91268aae19
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-29 15:11:01 -0300

    add feature favoritos

---
+++ Pokedex/Favorites/Cell/FavoritesCollectionViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  FavoritesCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Felipe Almeida on 07/09/22.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "favoritesCollectionViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: nil)
+
+    }
+    
+    @IBOutlet var pokemonImageView: UIImageView!
+    
+    @IBOutlet var pokemonNameLabel: UILabel!
+    
+    @IBOutlet var pokemonNumberLabel: UILabel!
+    
+    @IBOutlet var cellBackgroundView: UIView!
+    
+    @IBOutlet var pokemonTypeImageView: UIImageView!
+    
+    @IBOutlet var shadowView: UIView!
+    
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initialConfig()
+        shadowName()
+        shadowNumber()
+        setGradient()
+    }
+    
+    
+    
+    override func layoutSublayers(of layer: CALayer) {
+        gradient.frame = cellBackgroundView.bounds
+    }
+
+    func shadowNumber() {
+        pokemonNumberLabel.layer.shadowColor = UIColor.black.cgColor
+        pokemonNumberLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        pokemonNumberLabel.layer.shadowOpacity = 0.8
+        pokemonNumberLabel.layer.shadowRadius = 15
+        pokemonNumberLabel.layer.makeShadow(color: .gray, x: 0, y: 4, blur: 4, spread: 0)
+    }
+    
+    func shadowName() {
+        pokemonNameLabel.layer.shadowColor = UIColor.black.cgColor
+        pokemonNameLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        pokemonNameLabel.layer.shadowOpacity = 0.8
+        pokemonNameLabel.layer.shadowRadius = 15
+        pokemonNameLabel.layer.makeShadow(color: .gray, x: 0, y: 4, blur: 4, spread: 0)
+    }
+    
+    func initialConfig() {
+        cellBackgroundView.layer.cornerRadius = 50
+        cellBackgroundView.layer.borderWidth = 1.5
+        cellBackgroundView.layer.borderColor = UIColor.black.cgColor
+
+        contentView.backgroundColor = .clear
+        layer.masksToBounds = false
+        cellBackgroundView.layer.borderColor = UIColor.black.cgColor
+        cellBackgroundView.clipsToBounds = true
+        
+        shadowView.clipsToBounds = true
+        shadowView.layer.cornerRadius = 55
+        shadowView.backgroundColor = .white
+        shadowView.layer.makeShadow(color: .darkGray, x: 0, y: 5, blur: 3, spread: 0)
+    }
+    
+    func setGradient() {
+        gradient.colors = [UIColor(red: 0/255, green: 191/255, blue: 255/255, alpha: 1).cgColor, UIColor.white.cgColor]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
+        gradient.locations = [NSNumber(floatLiteral: 0.0), NSNumber(floatLiteral: 1.0)]
+        gradient.shouldRasterize = true
+        gradient.frame = cellBackgroundView.bounds
+        
+        cellBackgroundView.layer.addSublayer(gradient)
+    }
+}
---
+++ Pokedex/Favorites/FavoritesViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Gabriel on 03/09/22.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var favoritesView: UIView!
+    
+    @IBOutlet weak var favoritesLabel: UILabel!
+    
+    @IBOutlet weak var favoritesCollectionView: UICollectionView!
+    
+    struct PokedexFavorites {
+        let name: String
+        let imageFavorite: String
+    }
+    
+    let data: [PokedexFavorites] = [PokedexFavorites(name: "Lapras", imageFavorite: "laprasfavo")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        favoritesCollectionView.register(FavoritesCollectionViewCell.nib(), forCellWithReuseIdentifier: FavoritesCollectionViewCell.identifier)
+        favoritesCollectionView.delegate = self
+        favoritesCollectionView.dataSource = self
+        cornerRadiusView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavoritesCollectionViewCell.identifier, for: indexPath)
+        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.size.width - 20, height: 120)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "PokemonSelectedVc", bundle: nil)
+        let viewcontroler = storyboard.instantiateViewController(withIdentifier: "PokemonSelectedVc")
+        navigationController?.pushViewController(viewcontroler, animated: true)
+    }
+    
+    func cornerRadiusView() {
+        favoritesView.layer.cornerRadius = 40
+        favoritesView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        favoritesView.layer.borderWidth = 2.0
+        favoritesView.layer.borderColor = UIColor.black.cgColor
+        favoritesView.clipsToBounds = true
+        
+        favoritesLabel.layer.shadowRadius = 3.5
+        favoritesLabel.layer.shadowOffset = CGSize(width: 4, height: 4)
+        favoritesLabel.layer.shadowOpacity = 1.0
+        favoritesLabel.layer.masksToBounds = false
+        favoritesLabel.layer.shadowColor = UIColor.black.cgColor
+    }
+}
+
