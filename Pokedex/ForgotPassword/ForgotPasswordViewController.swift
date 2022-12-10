667d90ec50c15c3d61383e351085e95b5c381149
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-12-10 16:10:05 -0300

    trocando nomes para ingles - ForgotPassword

--- Pokedex/ForgotPassword/ForgotPasswordViewController.swift
+++ Pokedex/ForgotPassword/ForgotPasswordViewController.swift
@@ -7,22 +7,18 @@
 
 import UIKit
 
-class EsqueceuSenhaViewController: UIViewController, UITextFieldDelegate {
-
+class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var sentEmailButton: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sentEmailButton.layer.cornerRadius = 15
         emailTextField.delegate = self
-       
     }
-
+    
     @IBAction func tappedBackButton(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
-    
     }
 }
